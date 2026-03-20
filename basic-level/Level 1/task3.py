12c3347e1367992f4a4c05259503e7cf963aa1d3
Himanshu <169117364+imphenomenal07@users.noreply.github.com>
2026-03-20 12:35:14 +0530

    3️. Add two numbers (input from user)

---
+++ basic-level/Level 1/task3.py
@@ -0,0 +1,9 @@
+# 3️. Add two numbers (input from user)
+
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number: "))
+
+result = num1+num2
+
+print("The sum of both numbers is", result)
